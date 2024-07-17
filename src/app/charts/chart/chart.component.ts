c2d46d87d62d1b0fc25c219a0713fa36e664cf32
RowanRagab <Rowanmahran47@gmail.com>
2024-07-17 15:45:34 +0300

    update file extention

--- src/app/charts/chart/chart.component.ts
+++ src/app/charts/chart/chart.component.ts
@@ -6,7 +6,7 @@ Chart.register(...registerables);
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnChanges, OnDestroy {
   @Input() transactions: any[] = [];
